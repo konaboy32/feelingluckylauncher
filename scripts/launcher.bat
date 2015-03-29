181d7d6dc68d7c80906a347390c515c2b17c7b48
Gareth Woodham <garethwoodham@gmail.com>
2015-03-29 19:26:02 +0200

    Improvements to bat file

--- scripts/launcher.bat
+++ scripts/launcher.bat
@@ -5,8 +5,10 @@ REM =========================================================
 
 REM =========================================================
 REM Configure these variables according to your system:
+set LAUNCHER_HOME=C:\Users\turtle\workspace\feelingluckylauncher\bin
 set MAME_HOME=G:\_ARCADE\EMULATORS\MAME148_HACK
 set ROMS_HOME=G:\_ARCADE\COMPLETE_ROM_SETS\mame148
 REM =========================================================
 
+cd %LAUNCHER_HOMe%
 java Launcher %MAME_HOME% %ROMS_HOME%
